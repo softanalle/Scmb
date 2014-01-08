cd0d7eb91dcdd2db5663ee0b8ef349deca2647bb
softanalle <tommi.rintala@gmail.com>
2014-01-08 21:49:48 +0200

    copyright update

--- Scmb/src/main/java/com/softanalle/scmb/ui/Preview.java
+++ Scmb/src/main/java/com/softanalle/scmb/ui/Preview.java
@@ -21,7 +21,28 @@ import java.util.List;
 
 /**
  * Created by t2r on 8.1.2014.
+ * @author Tommi Rintala <t2r@iki.fi>
+ * @license GNU GPL 3.0
+ *
+Area selector component for SCMB
+
+Copyright (C) 2013  Tommi Rintala <t2r@iki.fi>
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
+
+
 public class Preview extends SurfaceView implements SurfaceHolder.Callback {
     // logger instance
     private static final Logger logger = LoggerFactory.getLogger("Preview");
